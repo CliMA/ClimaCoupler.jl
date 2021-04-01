b014b822909d1992c6e1cf3bebba3df5630ba459
Ben Mackay <jbmackay@caltech.edu>
2021-04-01 12:16:45 -0700

    Fixes to documentation CI.

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<https://github.com/CliMA/CouplerMachine>",
+deploydocs(
+    repo = "<github.com/CliMA/CouplerMachine.jl.git>",
     push_preview = true,
-)=#
+)

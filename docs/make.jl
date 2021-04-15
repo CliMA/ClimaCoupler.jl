1bd0cf34eef4ffe2ec5cb1965d4f3c7da8ae91ee
Chris Hill <cnh@mit.edu>
2021-04-15 16:16:23 -0400

    Add literate (#5)
    * Placeholder for further docs
    
    Co-authored-by: Ben Mackay <jbmackay@caltech.edu>

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,36 @@
 using Documenter
 using CouplerMachine
+using Literate
+
+const EXPERIMENTS_DIR = joinpath(@__DIR__, "..", "experiments")
+const OUTPUT_DIR      = joinpath(@__DIR__, "..", "docs/src/generated")
+
+experiments = [
+               "DesignTests/simple_2testcomp.jl",
+              ]
+
+for experiment in experiments
+    experiment_filepath = joinpath(EXPERIMENTS_DIR, experiment)
+    Literate.markdown(experiment_filepath, OUTPUT_DIR, documenter=true)
+end
+
+experiment_pages = [
+                    "Simple Two Component Test" => "generated/simple_2testcomp.md",
+                   ]
+
+pages = Any[
+    "Home" => "index.md",
+    "Examples" => experiment_pages,
+    "Coupler Object" => "couplerstate.md",
+    "Coupled Timestepping" => "timestepping.md",
+]
+
 
 makedocs(
     sitename = "CouplerMachine",
     format = Documenter.HTML(),
-    modules = [CouplerMachine]
+    modules = [CouplerMachine],
+    pages = pages,
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
--- docs/src/couplerstate.md
+++ docs/src/couplerstate.md
@@ -1,5 +1,12 @@
 # Coupler Object
 
+The CouplerMachine defines a type ```CplState``` for a _container_ variable that holds information about the field 
+values that are being used to couple between components. Components can use a ```put!``` operation to 
+export a set of field values to a ```CplState``` variable. A ```get``` operation is used to retrieve
+a set field values from a ```CplState``` variable.
+
+## Coupler Object API
+
 ```@docs
     CouplerMachine.CplState
     CouplerMachine.register_cpl_field!

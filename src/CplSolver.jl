768e7f1f704c1ce267eb3d1b387225e96a6a2bea
Ben Mackay <jbmackay@caltech.edu>
2021-07-30 14:20:30 -0700

    Add concurrent coupler prototype

--- src/CplSolver.jl
+++ src/CplSolver.jl
@@ -1,7 +1,21 @@
 using ClimateMachine.ODESolvers
 using ClimateMachine.ODESolvers: AbstractODESolver
+using Base.Threads
 
-export CplSolver
+export SerialCplSolver, ConcurrentCplSolver
+
+"""
+    AbstractCplSolver
+
+An abstract type for coupled solver schemes.
+
+At present, this extends ClimateMachine's AbstractODESolver interface.
+Concrete implementations currently include `SerialCplSolver` and `ConcurrentCplSolver`. 
+Coupling schemes such as implicit schemes or higher order explicit schemes may need
+to store additional info such as convergence tolerances or substage history.
+"""
+# TODO: may not tie this to the AbstractODESolver in the future. Depends on outer time loop mechanism
+abstract type AbstractCplSolver <: AbstractODESolver end
 
 """
     CplSolver(; component_list, coupler::CplState, coupling_dt, t0)
@@ -20,35 +34,33 @@ later use by other components. The `CplSolver` abstraction controls
  2. the execution of actions mapping exports from one or more components to imports of
     other components through an intermediary coupler name space.
 """
-mutable struct CplSolver{CL, FT, FL} <: AbstractODESolver
-    "Named list of pre-defined components"
-    component_list::CL
-    "Coupler State"
-    coupler::CplState
-    "Coupling timestep"
-    dt::FT
-    "Start time - initializes or tries to restart"
-    t0::FT
-    "Current time"
-    t::FT
-    "elapsed number of steps"
-    steps::Int
-    "Flux Log"
-    fluxlog::FL
+
+"""
+    SerialCplSolver(; component_list, coupler, coupling_dt, t0, fluxlog)
+
+A sequential-leapfrog coupling scheme.
+"""
+mutable struct SerialCplSolver{CL, FT, FL} <: AbstractCplSolver
+    component_list::CL # Named list of pre-defined components
+    coupler::CplState # Coupler State
+    dt::FT # Coupling timestep
+    t0::FT # Start time - initializes or tries to restart
+    t::FT # Current time
+    steps::Int # elapsed number of steps
+    fluxlog::FL # Flux Log
 end
 
-function CplSolver(;
+function SerialCplSolver(;
     component_list = component_list,
     coupler::CplState = coupler,
     coupling_dt = coupling_dt,
     t0 = t0,
     fluxlog = nothing,
 )
-    return CplSolver(component_list, coupler, coupling_dt, t0, t0, 0, fluxlog)
+    return SerialCplSolver(component_list, coupler, coupling_dt, t0, t0, 0, fluxlog)
 end
 
-function ODESolvers.dostep!(Qtop, csolver::CplSolver, param, time::Real)
-
+function ODESolvers.dostep!(Qtop, csolver::SerialCplSolver, param, time::Real)
     for cpl_component in csolver.component_list
 
         # pre_step fetching imports goes here
@@ -64,6 +76,53 @@ function ODESolvers.dostep!(Qtop, csolver::CplSolver, param, time::Real)
         # post step pushing exports goes here
         cpl_component.post_step(csolver)
 
+    end
+    return nothing
+end
+
+"""
+    ConcurrentCplSolver(; component_list, coupler, coupling_dt, t0, fluxlog)
+
+A parallel explicit coupling scheme using Julia's multithreading.
+"""
+mutable struct ConcurrentCplSolver{CL, FT, FL} <: AbstractCplSolver
+    component_list::CL # Named list of pre-defined components
+    coupler::CplState # Coupler State
+    dt::FT # Coupling timestep
+    t0::FT # Start time - initializes or tries to restart
+    t::FT # Current time
+    steps::Int # elapsed number of steps
+    fluxlog::FL # Flux Log
+end
+
+function ConcurrentCplSolver(;
+    component_list = component_list,
+    coupler::CplState = coupler,
+    coupling_dt = coupling_dt,
+    t0 = t0,
+    fluxlog = nothing,
+)
+    return ConcurrentCplSolver(component_list, coupler, coupling_dt, t0, t0, 0, fluxlog)
+end
+
+function ODESolvers.dostep!(Qtop, csolver::ConcurrentCplSolver, param, time::Real)
+    @threads for cpl_component in csolver.component_list
+        # pre_step fetching imports goes here
+        cpl_component.pre_step(csolver)
+    end
+    @threads for cpl_component in csolver.component_list
+            component = cpl_component[:component_model]
+            solve!(
+            component.state,
+            component.odesolver,
+            param;
+            numberofsteps = component.nsteps,
+            callbacks = component.callbacks,
+        )
+    end
+    @threads for cpl_component in csolver.component_list
+        # post step pushing exports goes here
+        cpl_component.post_step(csolver)
     end
     return nothing
 end

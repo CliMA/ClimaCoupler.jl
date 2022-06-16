708f3b702d8f7c5157d1f2fab86ac6a62b72b66d
Ben Mackay <jbmackay@caltech.edu>
2022-06-16 13:44:15 -0700

    Update tests for current interface changes.

--- test/CouplerState/cplstate_interface.jl
+++ test/CouplerState/cplstate_interface.jl
@@ -1,16 +1,25 @@
 using Test
 using Random
 using ClimaCoupler, Dates, Unitful
+using IntervalSets
+using ClimaCore: Domains, Meshes, Topologies, Spaces, Fields
+
+struct SimulationA <: ClimaCoupler.AbstractSimulation end
+ClimaCoupler.name(::SimulationA) = :simA
+
+struct SimulationB <: ClimaCoupler.AbstractSimulation end
+ClimaCoupler.name(::SimulationB) = :simB
 
 @testset "Coupler Interface" begin
     Random.seed!(26)
 
+    simA = SimulationA()
+    simB = SimulationB()
     coupler = CouplerState()
 
     data = rand(10, 10)
-    coupler_add_field!(coupler, :test1, data)
-    coupler_add_field!(coupler, :test2, data)
-    coupler_add_field!(coupler, :test3, data)
+    coupler_add_field!(coupler, :test1, data; write_sim = simA)
+    # coupler_add_field!(coupler, :test2, data; write_sim = simB)
 
     @show coupler
 
@@ -23,15 +32,18 @@ using ClimaCoupler, Dates, Unitful
 
     @testset "coupler_put!" begin
         newdata = rand(10, 10)
-        coupler_put!(coupler, :test1, newdata)
-
+        # simA can write to :test1
+        coupler_put!(coupler, :test1, newdata, simA)
         @test newdata == coupler_get(coupler, :test1)
         # coupler_put! is in-place; original data array has been modified
         @test data === coupler_get(coupler, :test1)
 
-        # coupler_put! must be to a previously add_fielded field
-        @test_throws KeyError coupler_put!(coupler, :idontexist, newdata)
-        # incoming data must match dimensions of add_fielded field
-        @test_throws DimensionMismatch coupler_put!(coupler, :test1, rand(10, 5))
+        # simB cannot write to :test1
+        @test_throws AssertionError coupler_put!(coupler, :test1, newdata, simB)
+
+        # coupler_put! must be to a previously added field
+        @test_throws KeyError coupler_put!(coupler, :idontexist, newdata, simA)
+        # incoming data must match dimensions of added field
+        @test_throws DimensionMismatch coupler_put!(coupler, :test1, rand(10, 5), simA)
     end
 end

d40edec5531d6bdac7026442240b69bd9f84bca4
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-10-30 17:03:24 -0700

    Move artifacts to its own folder
    Add artifacts job
    
    Add ArtifactWrappers to experiments env
    
    Add and use artifacts env

---
+++ artifacts/download_artifacts.jl
@@ -0,0 +1,10 @@
+include(joinpath(@__DIR__, "artifact_funcs.jl"))
+
+# Trigger download if data doesn't exist locally
+function trigger_download()
+    @info "sst dataset path: `$(sst_dataset_path())`"
+    @info "sic dataset path: `$(sic_dataset_path())`"
+    @info "mask dataset path: `$(mask_dataset_path())`"
+    return nothing
+end
+trigger_download()

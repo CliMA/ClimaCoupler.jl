dbbc23e6bf975640d96c81087717f800dd000b22
Julia Sloan <jsloan@caltech.edu>
2022-12-14 08:22:29 -0800

    SafeTestsets used

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
-include("CoupledSimulations/clock.jl")
-include("CouplerState/cplstate_interface.jl")
+using SafeTestsets
+
+@safetestset "Clock tests" begin
+    include("CoupledSimulations/clock.jl")
+end
+@safetestset "CouplerState tests" begin
+    include("CouplerState/cplstate_interface.jl")
+end
+@safetestset "Regridder tests" begin
+    include("regridder_tests.jl")
+end
+@safetestset "ConservationChecker tests" begin
+    include("conservation_checker_tests.jl")
+end
 # include("CoupledSimulations/cplsolver.jl")
-include("regridder_tests.jl")
-include("conservation_checker_tests.jl")

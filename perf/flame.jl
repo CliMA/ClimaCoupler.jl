d96b25358d1bd1755ea94d512c21ee56b03cd077
Valeria Barra <valeriabarra21@gmail.com>
2023-02-09 14:55:06 -0800

    Add allocation percentage change info string

--- perf/flame.jl
+++ perf/flame.jl
@@ -98,6 +98,12 @@ end
 
 Δallocs = allocs / allocs_limit[run_name]
 @info "Allocation change (allocs/allocs_limit): $Δallocs"
+percent_alloc_change = (1 - Δallocs) * 100
+if percent_alloc_change ≥ 0
+    @info "Allocations improved by: $percent_alloc_change %"
+else
+    @info "Allocations worsened by: $percent_alloc_change %"
+end
 
 @testset "Allocations limit" begin
     @test allocs ≤ allocs_limit[run_name]

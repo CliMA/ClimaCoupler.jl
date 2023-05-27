24e6152bbcb9846c84be88e8c0e5d0673490a59e
Julia Sloan <jsloan@caltech.edu>
2023-05-26 17:48:19 -0700

    rm exp dirs

--- experiments/AMIP/moist_mpi_aquaplanet/coupler_utils/regridder.jl
+++
@@ -1,4 +0,0 @@
-# for AMIP we don't need regridding. WHen we do we re-introduce the ClimaCoreTempestRemap 
-function dummmy_remap!(target, source)  # TODO: bring back Tempest regrid
-    parent(target) .= parent(source)
-end

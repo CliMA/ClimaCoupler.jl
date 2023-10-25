d07f3025223cffc43f7494b823506cff7a57e31b
LenkaNovak <lenka@caltech.edu>
2023-10-24 22:52:35 -0700

    fiz atmos standalone

--- target/atmos_driver.jl
+++ target/atmos_driver.jl
@@ -1,3 +1,25 @@
-include("mpi/mpi_init.jl") # setup MPI context for distributed runs #hide
+using Pkg
+Pkg.activate("../experiments/AMIP/modular/")
 
-include(joindir(pkgdir(CA), "examples/hybrid/driver.jl"))
+using ClimaComms
+using Logging
+using ClimaAtmos
+
+const is_distributed = get(ENV, "CLIMACORE_DISTRIBUTED", "") == "MPI"
+if is_distributed
+    const comms_ctx = ClimaComms.context(ClimaComms.CPUSingleThreaded())
+    const pid, nprocs = ClimaComms.init(comms_ctx)
+
+    if ClimaComms.iamroot(comms_ctx)
+        Logging.global_logger(Logging.ConsoleLogger(stderr, Logging.Info))
+    else
+        Logging.global_logger(Logging.NullLogger())
+    end
+else
+    using TerminalLoggers: TerminalLogger
+    const comms_ctx = ClimaComms.SingletonCommsContext()
+    const pid, nprocs = ClimaComms.init(comms_ctx)
+end
+
+Pkg.activate(joinpath(pkgdir(ClimaAtmos), "examples"))
+include(joinpath(pkgdir(ClimaAtmos), "examples/hybrid/driver.jl"))

ee14d59b1526c304645ce3003fa267c7c8212099
LenkaNovak <lenka@caltech.edu>
2023-10-28 05:20:10 -0700

    ρc_soil = FT(2e8), add topo, ocean z0

--- experiments/AMIP/modular/components/ocean/slab_ocean_init.jl
+++ experiments/AMIP/modular/components/ocean/slab_ocean_init.jl
@@ -86,7 +86,7 @@ Initializes the `DiffEq` problem, and creates a Simulation-type object containin
 function ocean_init(::Type{FT}; tspan, dt, saveat, space, area_fraction, thermo_params, stepper = CTS.RK4(), evolving = true) where {FT}
 
     evolving_switch =  evolving ? FT(1) : FT(0)
-    params = OceanSlabParameters(FT(20), FT(1500.0), FT(800.0), FT(271.0), FT(1e-3), FT(1e-5), FT(0.38), evolving_switch)
+    params = OceanSlabParameters(FT(20), FT(1500.0), FT(800.0), FT(271.0), FT(1e-5), FT(1e-5), FT(0.38), evolving_switch)
 
     Y, space = slab_ocean_space_init(FT, space, params)
     cache = (

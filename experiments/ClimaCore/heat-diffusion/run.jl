283d902da81573e801658ef78a2c7faff185726c
Julia Sloan <jsloan@caltech.edu>
2023-11-21 16:01:32 -0800

    exp/ClimaCore/ cleanup

--- experiments/ClimaCore/heat-diffusion/run.jl
+++ experiments/ClimaCore/heat-diffusion/run.jl
@@ -62,6 +62,10 @@
 #  - `T1` is the atm temperature near the surface (here assumed equal to the first model level)
 #  - `λ` a constant relaxation timescale
 
+# The atmosphere's surface flux, `atm_F_sfc`, is calculated by the atmosphere's timestepping.
+# It is then passed to the land model via the coupler. The land model's `lnd_F_sfc` gets updated
+# with this value from the atmosphere, but the surface flux is not calculated by the land model itself.
+
 # Note that in a more realistic setup the above equations would be weighted by their domains' densities and thermal heat capacities, so that the thermal flux would have the units of W m$^{-2}$.
 # Here we assume these are unity for both domains.
 
@@ -85,16 +89,15 @@
 #  - load external packages:
 import LinearAlgebra
 import ClimaCore: Fields, Domains, Topologies, Meshes, DataLayouts, Operators, Geometry, Spaces
+import ClimaTimeSteppers as CTS
 
 using Base: show_supertypes
-using OrdinaryDiffEq: ODEProblem, solve, SSPRK33
+import SciMLBase: step!, ODEProblem, init
 
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 
 using RecursiveArrayTools
-
-using OrdinaryDiffEq
 
 using Statistics
 
@@ -145,19 +148,25 @@ We also use this model to calculate and accumulate the downward surface fluxes, 
         F_integrated is reset to 0 at the beginning of each coupling cycle
         T1 = atm temperature near the surface (here assumed equal to the first model level)
 """
-function ∑tendencies_atm!(du, u, (parameters, T_sfc), t)
-    T = u.x[1] # u.x = vector of prognostic variables from DifferentialEquations
-    F_sfc = calculate_flux(T_sfc[1], parent(T)[1], parameters)
-    ## set BCs
-    bcs_bottom = Operators.SetValue(Geometry.Cartesian3Vector(F_sfc)) # F_sfc is converted to a Cartesian vector in direction 3 (vertical)
-    bcs_top = Operators.SetValue(FT(parameters.T_top))
+function ∑tendencies_atm!(du, u, cache, t)
+    parameters, T_sfc = cache
+    T_atm = u.T_atm
+    F_sfc = calculate_flux(T_sfc[1], parent(T_atm)[1], parameters)
 
-    gradc2f = Operators.GradientC2F(top = bcs_top) # Dirichlet BC (center-to-face)
-    gradf2c = Operators.DivergenceF2C(bottom = bcs_bottom) # Neumann BC (face-to-center)
+    ## set boundary conditions
+    C3 = Geometry.WVector
+    # note: F_sfc is converted to a Cartesian vector in direction 3 (vertical)
+    bcs_bottom = Operators.SetValue(C3(F_sfc))
+    bcs_top = Operators.SetValue(C3(FT(0)))
 
-    ## tendency calculations
-    @. du.x[1] = gradf2c(parameters.μ * gradc2f(T)) # dT/dt
-    du.x[2] .= -F_sfc[1] # d(F_integrated)/dt
+    ## gradient and divergence operators needed for diffusion in tendency calc.
+    ᶠgradᵥ = Operators.GradientC2F()
+    ᶜdivᵥ = Operators.DivergenceF2C(bottom = bcs_bottom, top = bcs_top)
+
+    ## perform tendency calculations
+    # note: `atm_F_sfc` is a prognostic variable (i.e. is timestepped) in the atmosphere model
+    @. du.T_atm = ᶜdivᵥ(parameters.μ * ᶠgradᵥ(T_atm)) # dT/dt
+    @. du.atm_F_sfc = -F_sfc[1] # d(F_integrated)/dt
 end
 
 # - Model 2 (lnd) Equations
@@ -202,15 +211,15 @@ ics = (; atm = T_atm_0, lnd = T_lnd_0)
 stepping = (;
     Δt_min = 0.02,
     timerange = (0.0, 6.0),
-    Δt_coupler = 1.0,
-    odesolver = SSPRK33(),
+    Δt_coupler = 0.1,
+    odesolver = CTS.ExplicitAlgorithm(CTS.RK4()),
     nsteps_atm = 8, # number of timesteps of atm per coupling cycle
     nsteps_lnd = 1, # number of timesteps of lnd per coupling cycle
 );
 
 # ## Define the sequential coupling loop
 function coupler_solve!(stepping, ics, parameters)
-    t = 0.0
+    ## access start time, end time, and timesteps
     Δt_min = stepping.Δt_min
     Δt_coupler = stepping.Δt_coupler
     t_start = stepping.timerange[1]
@@ -227,39 +236,51 @@ function coupler_solve!(stepping, ics, parameters)
     ## SETUP ATMOS
     ## put all prognostic variable arrays into a vector and ensure that solve can partition them
     T_atm = ics.atm
-    Y_atm = ArrayPartition((T_atm, atm_F_sfc))
-    prob_atm = ODEProblem(∑tendencies_atm!, Y_atm, (t_start, t_end), (parameters, atm_T_lnd))
-    integ_atm = init(prob_atm, stepping.odesolver, dt = Δt_min, saveat = 10 * Δt_min)
+    Y_atm = Fields.FieldVector(T_atm = T_atm, atm_F_sfc = atm_F_sfc)
+
+    ode_algo = stepping.odesolver
+    ode_function = CTS.ClimaODEFunction(T_exp! = ∑tendencies_atm!)
+
+    # `saveat` determines the frequency with which the solution is saved
+    # `adaptive` determines whether the solver should use an adaptive timestep
+    prob_atm = ODEProblem(ode_function, Y_atm, (t_start, t_end), (parameters = parameters, T_sfc = atm_T_lnd))
+    integ_atm = init(prob_atm, ode_algo, dt = Δt_min, saveat = 10 * Δt_min, adaptive = false)
 
     ## land copies of coupler variables
+    # note: `lnd_F_sfc` is a diagnostic variable (i.e. is not timestepped) in the land model
     T_lnd = ics.lnd
     lnd_F_sfc = copy(coupler_F_sfc)
 
     ## SETUP LAND
-    prob_lnd = ODEProblem(∑tendencies_lnd!, T_lnd, (t_start, t_end), (parameters, lnd_F_sfc))
-    integ_lnd = init(prob_lnd, stepping.odesolver, dt = Δt_min, saveat = 10 * Δt_min)
+    ode_function = CTS.ClimaODEFunction(T_exp! = ∑tendencies_lnd!)
+
+    prob_lnd = ODEProblem(ode_function, T_lnd, (t_start, t_end), (parameters, lnd_F_sfc))
+    integ_lnd = init(prob_lnd, ode_algo, dt = Δt_min, saveat = 10 * Δt_min, adaptive = false)
+
 
     ## coupler stepping
-    for t in (t_start:Δt_coupler:t_end)
+    for t in ((t_start + Δt_coupler):Δt_coupler:t_end)
 
         ## STEP ATMOS
         ## pre_atmos
-        integ_atm.p[2] .= coupler_get_(coupler_T_lnd) # integ_atm.p is the parameter vector of an ODEProblem from DifferentialEquations
-        integ_atm.u.x[2] .= [0.0] # surface flux to be accumulated
+        integ_atm.p[2] .= coupler_get_(coupler_T_lnd) # integ_atm.p is the parameter vector of an ODEProblem from SciMLBase
+        integ_atm.u.atm_F_sfc .= 0.0 # surface flux to be accumulated
 
         ## run atmos
         ## NOTE: use (t - integ_atm.t) here instead of Δt_coupler to avoid accumulating roundoff error in our timestepping.
-        OrdinaryDiffEq.step!(integ_atm, t - integ_atm.t, true)
+        step!(integ_atm, t - integ_atm.t, true)
 
         ## post_atmos
-        coupler_F_sfc .= coupler_put_(integ_atm.u.x[2]) / Δt_coupler
+        ## retrieve the updated surface flux from the atmosphere model
+        coupler_F_sfc .= coupler_put_(integ_atm.u.atm_F_sfc) / Δt_coupler
 
         ## STEP LAND
         ## pre_land
+        ## provide the updated surface flux to the land model
         lnd_F_sfc .= coupler_get_(coupler_F_sfc)
 
         ## run land
-        OrdinaryDiffEq.step!(integ_lnd, t - integ_lnd.t, true)
+        step!(integ_lnd, t - integ_lnd.t, true)
 
         ## post land
         coupler_T_lnd .= coupler_put_(integ_lnd.u) # update T_sfc
@@ -274,12 +295,13 @@ sol_atm, sol_lnd = integ_atm.sol, integ_lnd.sol;
 
 # ## Postprocessing and Visualization
 
-# Each integrator output (`sol_atm`, `sol_lnd`), contains the DifferentialEquations variable `.u` (the name is hard coded).
-# If `ArrayPartition` was used for combining multiple prognostic variables, `u` will include an additional variable `x` (also hard coded)
+# Each integrator output (`sol_atm`, `sol_lnd`), contains the SciMLBase variable `.u` (the name is hard coded).
+# If multiple prognostic variables were solved for, `u` will contain all of those.
 # `parent()` accesses the `Field` values.
-# So, for example, the structure of `u` from Model 1 is:
-
-# `parent(sol_atm.u[<time-index>].x[<ArrayPartition-index>])[<z-index>,<variable-index>]`
+# So, for example, the structure of `u` from the atmosphere model is:
+# `parent(sol_atm.u[<time-index>].T_atm)[<z-index>,<variable-index>]`
+# and
+# `parent(sol_atm.u[<time-index>].atm_F_sfc)[<z-index>,<variable-index>]`
 
 ENV["GKSwstype"] = "nul"
 import Plots
@@ -291,8 +313,8 @@ path = joinpath(dirname(@__FILE__), "images/")
 mkpath(path);
 
 # - Vertical profile at start and end
-t0_ = parent(sol_atm.u[1].x[1])[:, 1];
-tend_ = parent(sol_atm.u[end].x[1])[:, 1];
+t0_ = parent(sol_atm.u[1].T_atm)[:, 1];
+tend_ = parent(sol_atm.u[end].T_atm)[:, 1];
 z_centers = parent(Fields.coordinate_field(center_space_atm))[:, 1];
 show_plots ?
 Plots.png(
@@ -303,15 +325,17 @@ Plots.png(
         labels = ["t=0" "t=end"],
         xlabel = "T (K)",
         ylabel = "z (m)",
+        linewidth = 2,
     ),
-    joinpath(path, "tc1_f1.png"),
+    joinpath(path, "heat-diffusion_f1.png"),
 ) : nothing
-# ![](images/tc1_f1.png)
+# ![](images/heat-diffusion_f1.png)
 
 # - Conservation: absolute "energy" of both models with time
 # convert to the same units (analogous to energy conservation, assuming that is both domains density=1 and thermal capacity=1)
 lnd_sfc_u_t = [u[1] for u in sol_lnd.u] .* parameters.h_lnd;
-atm_sum_u_t = [sum(parent(u.x[1])[:]) for u in sol_atm.u] .* (parameters.zmax_atm - parameters.zmin_atm) ./ parameters.n;
+atm_sum_u_t =
+    [sum(parent(u.T_atm)[:]) for u in sol_atm.u] .* (parameters.zmax_atm - parameters.zmin_atm) ./ parameters.n;
 v1 = lnd_sfc_u_t .- lnd_sfc_u_t[1];
 v2 = atm_sum_u_t .- atm_sum_u_t[1];
 show_plots ?
@@ -322,24 +346,33 @@ Plots.png(
         labels = ["lnd" "atm" "tot"],
         xlabel = "time (s)",
         ylabel = "pseudo-energy (J / m2)",
+        linewidth = 2,
     ),
-    joinpath(path, "tc1_f2.png"),
+    joinpath(path, "heat-diffusion_f2.png"),
 ) : nothing
-# ![](images/tc1_f2.png)
+# ![](images/heat-diffusion_f2.png)
 
 # - Conservation: relative error with time
 total = atm_sum_u_t + lnd_sfc_u_t;
-rel_error = (total .- total[1]) / mean(total);
+rel_error = abs.(total .- total[1]) / mean(total);
 show_plots ?
 Plots.png(
-    Plots.plot(sol_lnd.t, rel_error, labels = ["tot"], xlabel = "time (s)", ylabel = "relative error"),
-    joinpath(path, "tc1_f3.png"),
+    Plots.plot(
+        sol_lnd.t,
+        rel_error,
+        labels = nothing,
+        xlabel = "simulation time (s)",
+        ylabel = "relative error",
+        title = "Total Energy Conservation",
+        linewidth = 2,
+    ),
+    joinpath(path, "heat-diffusion_f3.png"),
 ) : nothing
-# ![](images/tc1_f3.png)
+# ![](images/heat-diffusion_f3.png)
 
 #src # - Animation
 #src anim = Plots.@animate for u in sol_atm.u
-#src     Plots.plot(u.x[1], xlim=(220,280))
+#src     Plots.plot(u.T_atm, xlim=(220,280))
 #src end
 #src Plots.mp4(anim, joinpath(path, "heat.mp4"), fps = 10)
 #src

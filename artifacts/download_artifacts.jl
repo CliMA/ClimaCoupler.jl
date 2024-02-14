8872e68900edb4dce5c4d67634ce9aad056f2136
Gabriele Bozzola <sbozzolator@gmail.com>
2024-02-13 16:24:29 -0800

    Add infrastructure to compute errors against obs

--- artifacts/download_artifacts.jl
+++ artifacts/download_artifacts.jl
@@ -23,6 +23,7 @@ function trigger_download()
     @info "sic dataset path: `$(sic_dataset_path())`"
     @info "co2 dataset path: `$(co2_dataset_path())`"
     @info "mask dataset path: `$(mask_dataset_path())`"
+    @info "pr obs data path: `$(pr_obs_data_path())`"
     return nothing
 end
 trigger_download()

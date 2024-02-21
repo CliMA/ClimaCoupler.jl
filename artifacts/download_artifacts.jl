31adbb839182af54b3482af9867a7330fbd16eab
Gabriele Bozzola <sbozzolator@gmail.com>
2024-02-21 15:37:02 -0800

    Download Atmos/Land artifacts too

--- artifacts/download_artifacts.jl
+++ artifacts/download_artifacts.jl
@@ -1,3 +1,20 @@
+
+# Download atmos artifacts
+module Atmos
+import ClimaAtmos
+include(joinpath(pkgdir(ClimaAtmos), "artifacts", "download_artifacts.jl"))
+trigger_download()
+end
+
+# Download land artifacts
+module Land
+import ClimaLand
+include(joinpath(pkgdir(ClimaLand), "src", "standalone", "Bucket", "artifacts", "artifacts.jl"))
+@info "CESM", cesm2_albedo_dataset_path()
+@info "Bareground albedo", bareground_albedo_dataset_path()
+end
+
+
 include(joinpath(@__DIR__, "artifact_funcs.jl"))
 
 # Trigger download if data doesn't exist locally

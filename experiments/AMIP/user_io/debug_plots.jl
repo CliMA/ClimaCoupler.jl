a3cdf37051762b4ceaf424cab3c48a265e56c0df
Julia Sloan <jsloan@caltech.edu>
2024-03-19 14:59:48 -0700

    debug plots work cpu

--- experiments/AMIP/user_io/debug_plots.jl
+++ experiments/AMIP/user_io/debug_plots.jl
@@ -2,8 +2,8 @@ using Plots
 using ClimaCorePlots
 using Printf
 using ClimaCoupler.Interfacer: ComponentModelSimulation, SurfaceModelSimulation
+using ClimaCoupler: TestHelper
 using ClimaCore
-using Adapt
 
 # plotting functions for the coupled simulation
 """
@@ -45,28 +45,33 @@ function debug(cs_fields::NamedTuple, dir, cs_fields_ref = nothing)
     all_plots = []
     cpu_comms_ctx = ClimaComms.SingletonCommsContext()
 
-    # Extract information from input space to create space on CPU for plotting
-    field = getproperty(cs_fields, field_names[begin])
-    space = axes(field)
-    FT = ClimaCore.Spaces.undertype(space)
-    R = space.grid.global_geometry.radius
-    ne = space.grid.topology.mesh.ne
-    polynomial_degree = Quadratures.degrees_of_freedom(Spaces.quadrature_style(space.grid))
-    nz = Spaces.nlevels(space)
-
-    cpu_space = TestHelper.create_space(FT, comms_ctx = cpu_comms_ctx, R, ne, polynomial_degree, nz)
-    cpu_field = Fields.ones(cpu_space)
-
     for field_name in field_names
         field = getproperty(cs_fields, field_name)
+
         # Copy field onto cpu space
+        space = axes(field)
+        FT = ClimaCore.Spaces.undertype(space)
+        R = get_R(space.grid)
+        ne = get_ne(space.grid)
+        polynomial_degree = ClimaCore.Quadratures.polynomial_degree(ClimaCore.Spaces.quadrature_style(space.grid))
+        nz = ClimaCore.Spaces.nlevels(space)
+        cpu_space = TestHelper.create_space(
+            FT,
+            comms_ctx = cpu_comms_ctx,
+            R = R,
+            ne = ne,
+            polynomial_degree = polynomial_degree,
+            nz = nz,
+        )
+        cpu_field = ClimaCore.Fields.ones(cpu_space)
+
         parent(cpu_field) .= parent(field)
 
-        push!(all_plots, plot(cpu_field, title = string(field_name) * print_extrema(field)))
+        push!(all_plots, Plots.plot(cpu_field, title = string(field_name) * print_extrema(field)))
         if (field_name == :T_S) && (@isdefined debug_csf0)
             push!(
                 all_plots,
-                plot(
+                Plots.plot(
                     cpu_field .- debug_csf0.T_S,
                     title = string(field_name) * "_anom" * print_extrema(field),
                     color = :viridis,
@@ -74,7 +79,7 @@ function debug(cs_fields::NamedTuple, dir, cs_fields_ref = nothing)
             )
         end
     end
-    plot(all_plots..., size = (1500, 800))
+    Plots.plot(all_plots..., size = (1500, 800))
     Plots.png(joinpath(dir, "debug_coupler"))
 
     # plot anomalies if a reference cs.fields, `cs_fields_ref`, are provided
@@ -82,22 +87,38 @@ function debug(cs_fields::NamedTuple, dir, cs_fields_ref = nothing)
         all_plots = []
         for field_name in field_names
             field = getproperty(cs_fields, field_name)
-            # Convert field from GPU to CPU if necessary
-            cpu_field = Adapt.adapt(Array, field)
+            # Copy field onto cpu space
+            parent(cpu_field) .= parent(field)
 
             push!(
                 all_plots,
-                plot(
+                Plots.plot(
                     cpu_field .- getproperty(cs_fields_ref, field_name),
                     title = string(field_name) * print_extrema(field),
                 ),
             )
         end
-        plot(all_plots..., size = (1500, 800))
+        Plots.plot(all_plots..., size = (1500, 800))
         Plots.png(joinpath(dir, "debug_coupler_amomalies"))
     end
 end
 
+function get_ne(grid::ClimaCore.Grids.SpectralElementGrid2D)
+    return grid.topology.mesh.ne
+end
+function get_ne(grid::ClimaCore.Grids.LevelGrid)
+    return get_ne(grid.full_grid.horizontal_grid)
+end
+function get_ne(grid::ClimaCore.Grids.ExtrudedFiniteDifferenceGrid)
+    return get_ne(grid.horizontal_grid)
+end
+# function get_ne(space)
+#     return get_ne(ClimaCore.Spaces.horizontal_space(space).grid)
+# end
+
+function get_R(grid)#::ClimaCore.Grids.SpectralElementGrid2D)
+    return ClimaCore.Grids.global_geometry(grid).radius
+end
 """
     debug(sim::ComponentModelSimulation, dir)
 
@@ -105,17 +126,36 @@ Plot the fields of a component model simulation and save plots to a directory.
 """
 function debug(sim::ComponentModelSimulation, dir)
 
+    @show name(sim)
     field_names = plot_field_names(sim)
+    cpu_comms_ctx = ClimaComms.SingletonCommsContext()
 
     all_plots = []
     for field_name in field_names
         field = get_field(sim, Val(field_name))
-        # Convert field from GPU to CPU if necessary
-        cpu_field = Adapt.adapt(Array, field)
+        # Copy field onto cpu space
+        space = axes(field)
+        FT = ClimaCore.Spaces.undertype(space)
+        R = get_R(space.grid)
+        ne = get_ne(space.grid)
+        polynomial_degree = ClimaCore.Quadratures.polynomial_degree(ClimaCore.Spaces.quadrature_style(space.grid))
+        nz = ClimaCore.Spaces.nlevels(space)
+        cpu_space = TestHelper.create_space(
+            FT,
+            comms_ctx = cpu_comms_ctx,
+            R = R,
+            ne = ne,
+            polynomial_degree = polynomial_degree,
+            nz = nz,
+        )
+        cpu_field = ClimaCore.Fields.ones(cpu_space)
+        @show space
+        @show cpu_space
+        parent(cpu_field) .= parent(field)
 
-        push!(all_plots, plot(cpu_field, title = string(field_name) * print_extrema(field)))
+        push!(all_plots, Plots.plot(cpu_field, title = string(field_name) * print_extrema(field)))
     end
-    fig = plot(all_plots..., size = (1500, 800))
+    fig = Plots.plot(all_plots..., size = (1500, 800))
     Plots.png(joinpath(dir, "debug_$(name(sim))"))
 
 end

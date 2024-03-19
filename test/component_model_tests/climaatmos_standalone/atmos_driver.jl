ede7d4098809bfc243a767b6fd65289da141b745
Julia Sloan <jsloan@caltech.edu>
2024-03-19 08:21:49 -0700

    run atmos-only on buildkite

--- test/component_model_tests/climaatmos_standalone/atmos_driver.jl
+++ test/component_model_tests/climaatmos_standalone/atmos_driver.jl
@@ -1,11 +1,21 @@
 using ClimaComms
 using Logging
-using ClimaAtmos
 
 redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
 import ClimaAtmos as CA
 import Random
+using ClimaCoupler
 Random.seed!(1234)
+
+pkg_dir = pkgdir(ClimaCoupler)
+atmos_config_file = joinpath(pkg_dir, "test/component_model_tests/climaatmos_standalone/longrun_aquaplanet_dyamond.yml")
+comms_ctx = ClimaComms.context(ClimaComms.device())
+@show typeof(comms_ctx)
+
+config = CA.AtmosConfig(atmos_config_file; comms_ctx = comms_ctx)
+
+OUTPUT_DIR = joinpath(pkg_dir, "test/component_model_tests/climaatmos_standalone/output/longrun_aquaplanet_dyamond_artifacts/")
+mkpath(OUTPUT_DIR)
 
 if !(@isdefined config)
     config = CA.AtmosConfig()

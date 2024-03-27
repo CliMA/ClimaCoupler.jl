12dcbc25c2f9c35e72f5b8fc4b3b622b8b1f31e4
LenkaNovak <lenka@caltech.edu>
2024-03-27 10:20:43 -0700

    inst

--- test/component_model_tests/climaatmos_standalone/atmos_driver.jl
+++ test/component_model_tests/climaatmos_standalone/atmos_driver.jl
@@ -2,17 +2,17 @@ using ClimaComms
 using Logging
 using ClimaAtmos
 
-redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
-import ClimaAtmos as CA
-import Random
-Random.seed!(1234)
+# redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
+# import ClimaAtmos as CA
+# import Random
+# Random.seed!(1234)
 
-if !(@isdefined config)
-    config = CA.AtmosConfig()
-end
-simulation = CA.get_simulation(config)
-(; integrator) = simulation
-sol_res = CA.solve_atmos!(simulation)
+# if !(@isdefined config)
+#     config = CA.AtmosConfig()
+# end
+# simulation = CA.get_simulation(config)
+# (; integrator) = simulation
+# sol_res = CA.solve_atmos!(simulation)
 
 
 # ___
@@ -28,7 +28,6 @@ import Thermodynamics as TD
 import ClimaComms
 using SciMLBase
 using PrettyTables
-import DiffEqCallbacks as DECB
 using JLD2
 using NCDatasets
 using ClimaTimeSteppers

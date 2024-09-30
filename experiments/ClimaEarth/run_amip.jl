bf0ddfe16ac9e2ef69b0e885ecef2d75358c7b98
Julia Sloan <jsloan@caltech.edu>
2024-09-30 16:19:30 -0700

    include bucket diagnostics

--- experiments/ClimaEarth/run_amip.jl
+++ experiments/ClimaEarth/run_amip.jl
@@ -277,7 +277,7 @@ if mode_name == "amip"
         config_dict["land_domain_type"],
         config_dict["land_albedo_type"],
         config_dict["land_temperature_anomaly"],
-        dir_paths.regrid;
+        dir_paths;
         dt = Δt_cpl,
         space = boundary_space,
         saveat = saveat,
@@ -364,7 +364,7 @@ elseif mode_name in ("slabplanet", "slabplanet_aqua", "slabplanet_terra")
         config_dict["land_domain_type"],
         config_dict["land_albedo_type"],
         config_dict["land_temperature_anomaly"],
-        dir_paths.regrid;
+        dir_paths;
         dt = Δt_cpl,
         space = boundary_space,
         saveat = saveat,
@@ -413,7 +413,7 @@ elseif mode_name == "slabplanet_eisenman"
         config_dict["land_domain_type"],
         config_dict["land_albedo_type"],
         config_dict["land_temperature_anomaly"],
-        dir_paths.regrid;
+        dir_paths;
         dt = Δt_cpl,
         space = boundary_space,
         saveat = saveat,

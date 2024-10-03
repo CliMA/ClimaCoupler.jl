fdb77e7ae2cd09105842c656119899746cbc4901
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2024-10-03 16:02:21 -0700

    Refactor leaderboard code
    This commit refactors the leaderboard code using the new features from
    ClimaAnalysis. This commit also deletes the old leaderboard code and the
    tests for it. Also, there is an off by one month issue when handling the
    dates and seasons. This arises because the simulation data assigns the
    next month the monthly average for this month. For instance, the monthly
    average for January 2010 is assigned the date 2/1/2010. This is fixed in
    this commit.
    
    The commit also moves the code to leaderboard.jl and data_sources.jl.
    The file read_amip.jl calls leaderboard.jl to plot now. The conditional
    to run leaderboard.jl is changed from t_end > 86400 to t_end
    > 86400 * 30 since it doesn't make sense to run the code unless one or
    more month is done. This is because the dataset contains monthly
    averages.
    
    One significant difference is the leaderboard which now plot the best
    and worst single model using only annual rather than averaging the error
    over annual and seasonal data.

--- experiments/ClimaEarth/run_amip.jl
+++ experiments/ClimaEarth/run_amip.jl
@@ -920,115 +920,13 @@ if ClimaComms.iamroot(comms_ctx)
             output_dir = dir_paths.artifacts,
         )
 
-        ## Compare against observations
-        if t_end > 84600 && config_dict["output_default_diagnostics"]
-            @info "Error against observations"
-            include("user_io/leaderboard.jl")
-            ClimaAnalysis = Leaderboard.ClimaAnalysis
-
-            compare_vars_biases_plot_extrema = Dict(
-                "pr" => (-5.0, 5.0),
-                "rsdt" => (-2.0, 2.0),
-                "rsut" => (-50.0, 50.0),
-                "rlut" => (-50.0, 50.0),
-                "rsutcs" => (-20.0, 20.0),
-                "rlutcs" => (-20.0, 20.0),
-                "rsds" => (-50.0, 50.0),
-                "rsus" => (-10.0, 10.0),
-                "rlds" => (-50.0, 50.0),
-                "rlus" => (-50.0, 50.0),
-                "rsdscs" => (-10.0, 10.0),
-                "rsuscs" => (-10.0, 10.0),
-                "rldscs" => (-20.0, 20.0),
-            )
-
+        if t_end > 84600 * 30 && config_dict["output_default_diagnostics"]
+            include("leaderboard/leaderboard.jl")
             diagnostics_folder_path = atmos_sim.integrator.p.output_dir
             leaderboard_base_path = dir_paths.artifacts
-
-            compare_vars_biases_groups = [
-                ["pr", "rsdt", "rsut", "rlut"],
-                ["rsds", "rsus", "rlds", "rlus"],
-                ["rsutcs", "rlutcs", "rsdscs", "rsuscs", "rldscs"],
-            ]
-
-            function compute_biases(compare_vars_biases, dates)
-                if isempty(dates)
-                    return map(x -> 0.0, compare_vars_biases)
-                else
-                    return Leaderboard.compute_biases(
-                        diagnostics_folder_path,
-                        compare_vars_biases,
-                        dates,
-                        cmap_extrema = compare_vars_biases_plot_extrema,
-                    )
-                end
-            end
-
-            function plot_biases(dates, biases, output_name)
-                isempty(dates) && return nothing
-
-                output_path = joinpath(leaderboard_base_path, "bias_$(output_name).png")
-                Leaderboard.plot_biases(biases; output_path)
-            end
-
-            first_var = get(
-                ClimaAnalysis.SimDir(diagnostics_folder_path),
-                short_name = first(first(compare_vars_biases_groups)),
-            )
-
-            diagnostics_times = ClimaAnalysis.times(first_var)
-            # Remove the first `spinup_months` months from the leaderboard
-            spinup_months = 6
-            # The monthly average output is at the end of the month, so this is safe
-            spinup_cutoff = spinup_months * 31 * 86400.0
-            if diagnostics_times[end] > spinup_cutoff
-                filter!(x -> x > spinup_cutoff, diagnostics_times)
-            end
-
-            output_dates = Dates.DateTime(first_var.attributes["start_date"]) .+ Dates.Second.(diagnostics_times)
-            @info "Working with dates:"
-            @info output_dates
-            ## collect all days between cs.dates.date0 and cs.dates.date
-            MAM, JJA, SON, DJF = Leaderboard.split_by_season(output_dates)
-
-            for compare_vars_biases in compare_vars_biases_groups
-                ann_biases = compute_biases(compare_vars_biases, output_dates)
-                plot_biases(output_dates, ann_biases, first(compare_vars_biases) * "_total")
-
-                MAM_biases = compute_biases(compare_vars_biases, MAM)
-                plot_biases(MAM, MAM_biases, first(compare_vars_biases) * "_MAM")
-                JJA_biases = compute_biases(compare_vars_biases, JJA)
-                plot_biases(JJA, JJA_biases, first(compare_vars_biases) * "_JJA")
-                SON_biases = compute_biases(compare_vars_biases, SON)
-                plot_biases(SON, SON_biases, first(compare_vars_biases) * "_SON")
-                DJF_biases = compute_biases(compare_vars_biases, DJF)
-                plot_biases(DJF, DJF_biases, first(compare_vars_biases) * "_DJF")
-            end
-
-            compare_vars_rmses = ["pr", "rsut", "rlut"]
-
-            ann_biases = compute_biases(compare_vars_rmses, output_dates)
-            MAM_biases = compute_biases(compare_vars_rmses, MAM)
-            JJA_biases = compute_biases(compare_vars_rmses, JJA)
-            SON_biases = compute_biases(compare_vars_rmses, SON)
-            DJF_biases = compute_biases(compare_vars_rmses, DJF)
-
-            rmses = map(
-                (index) -> Leaderboard.RMSEs(;
-                    model_name = "CliMA",
-                    ANN = ann_biases[index],
-                    DJF = DJF_biases[index],
-                    MAM = MAM_biases[index],
-                    JJA = JJA_biases[index],
-                    SON = SON_biases[index],
-                ),
-                1:length(compare_vars_rmses),
-            )
-
-            Leaderboard.plot_leaderboard(rmses; output_path = joinpath(leaderboard_base_path, "bias_leaderboard.png"))
+            compute_leaderboard(leaderboard_base_path, diagnostics_folder_path)
         end
     end
-
     ## plot extra atmosphere diagnostics if specified
     if config_dict["ci_plots"]
         @info "Generating CI plots"

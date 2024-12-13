f5e7431771f80258a7740ae64aeb6cbe64256721
imreddyTeja <treddy0205@gmail.com>
2024-12-12 17:13:18 -0800

    Remove date-string conversion functions
    The strdate_to_datetime and datetime_to_strdate are unused
    after removing the Regridders module. Additionally, their funcitonality
    is contained within the Dates package with Dates.format and
    DateFormats.

--- src/TimeManager.jl
+++ src/TimeManager.jl
@@ -10,8 +10,6 @@ import Dates
 import ..Interfacer
 
 export current_date,
-    strdate_to_datetime,
-    datetime_to_strdate,
     trigger_callback,
     AbstractFrequency,
     Monthly,
@@ -32,31 +30,6 @@ Return the model date at the current timestep.
 - `t`: [Real] number of seconds since simulation began
 """
 current_date(cs::Interfacer.CoupledSimulation, t::Real) = cs.dates.date0[1] + Dates.Second(t)
-
-"""
-    strdate_to_datetime(strdate::String)
-
-Convert from String ("YYYYMMDD") to Date format,
-required by the official AMIP input files.
-
-# Arguments
-- `strdate`: [String] to be converted to Date type
-"""
-strdate_to_datetime(strdate::String) =
-    Dates.DateTime(parse(Int, strdate[1:4]), parse(Int, strdate[5:6]), parse(Int, strdate[7:8]))
-
-"""
-    datetime_to_strdate(datetime::DateTime)
-
-Convert from Date to String ("YYYYMMDD") format.
-
-# Arguments
-- `datetime`: [Dates.DateTime] object to be converted to string
-"""
-datetime_to_strdate(datetime::Dates.DateTime) =
-    string(lpad(Dates.year(datetime), 4, "0")) *
-    string(string(lpad(Dates.month(datetime), 2, "0"))) *
-    string(lpad(Dates.day(datetime), 2, "0"))
 
 """
     AbstractFrequency

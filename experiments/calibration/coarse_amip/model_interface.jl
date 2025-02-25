7c5a212e75977bb6907854802653e53c2df498dc
nefrathenrici <nat.henrici@gmail.com>
2025-02-25 14:03:15 -0800

    Running with single year batch, downsampling outputvars

---
+++ experiments/calibration/coarse_amip/model_interface.jl
@@ -0,0 +1,35 @@
+import ClimaCoupler
+import ClimaCalibrate
+import CUDA
+import EnsembleKalmanProcesses as EKP
+ENV["CLIMACOMMS_DEVICE"] = "CUDA"
+ENV["CLIMACOMMS_CONTEXT"] = "SINGLETON"
+import JLD2
+include(joinpath(pkgdir(ClimaCoupler), "experiments", "ClimaEarth", "setup_run.jl"))
+
+function ClimaCalibrate.forward_model(iter, member)
+
+    config_file = joinpath(pkgdir(ClimaCoupler), "experiments", "calibration", "coarse_amip", "model_config.yml")
+    config_dict = get_coupler_config_dict(config_file)
+
+    output_dir_root = config_dict["coupler_output_dir"]
+    eki = JLD2.load_object(ClimaCalibrate.ekp_path(output_dir_root, iter))
+    minibatch = EKP.get_current_minibatch(eki)
+    config_dict["start_date"] = minibatch_to_start_date(minibatch)
+
+    # Set member parameter file
+    sampled_parameter_file = ClimaCalibrate.parameter_path(output_dir_root, iter, member)
+    config_dict["calibration_toml"] = sampled_parameter_file
+    # Set member output directory
+    member_output_dir = ClimaCalibrate.path_to_ensemble_member(output_dir_root, iter, member)
+    config_dict["coupler_output_dir"] = member_output_dir
+
+    return setup_and_run(config_dict)
+end
+
+
+function minibatch_to_start_date(batch)
+    start_year = minimum(batch) + 1999
+    @assert start_year >= 2000
+    return "$(start_year)0901"
+end

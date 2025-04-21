8fff1ad8fa64d3e1b7cab69727b9f190d3945796
nefrathenrici <nat.henrici@gmail.com>
2025-04-21 09:15:48 -0700

    Fix calibration test, lower resource requirements

--- experiments/calibration/run_calibration.jl
+++ experiments/calibration/run_calibration.jl
@@ -5,14 +5,16 @@ using ClimaAnalysis
 import ClimaAnalysis: SimDir, get, slice, average_xy
 import ClimaComms
 import EnsembleKalmanProcesses: I, ParameterDistributions.constrained_gaussian
+import EnsembleKalmanProcesses as EKP
+using Statistics
 
 # Ensure ClimaComms doesn't use MPI
 ENV["CLIMACOMMS_CONTEXT"] = "SINGLETON"
 ClimaComms.@import_required_backends
 
-single_member_dims = (1,)
+single_member_dims = 1
 function CAL.observation_map(iteration)
-    G_ensemble = Array{Float64}(undef, single_member_dims..., ensemble_size)
+    G_ensemble = Array{Float64}(undef, single_member_dims, ensemble_size)
 
     for m in 1:ensemble_size
         member_path = CAL.path_to_ensemble_member(output_dir, iteration, m)
@@ -29,9 +31,8 @@ function CAL.observation_map(iteration)
 end
 
 function process_member_data(simdir::SimDir)
-    output = zeros(single_member_dims...)
+    output = zeros(single_member_dims)
     days = 86_400
-    isempty(simdir) && return NaN
 
     rsut = get(simdir; short_name = "rsut", reduction = "average", period = "30d")
     rsut_slice = slice(average_lon(average_lat(rsut)); time = 30days).data
@@ -54,7 +55,6 @@ addprocs(CAL.SlurmManager())
 end
 
 # Experiment Configuration
-ensemble_size = 10
 n_iterations = 5
 noise = 0.1 * I
 prior = constrained_gaussian("total_solar_irradiance", 1000, 500, 250, 2000)
@@ -68,14 +68,20 @@ if !isfile(obs_path)
     touch(joinpath(obs_output_dir, "parameters.toml"))
     CAL.forward_model(0, 0)
     observations = Vector{Float64}(undef, 1)
-    observations .= process_member_data(SimDir(joinpath(obs_output_dir, "amip_config/output_active/clima_atmos")))
+    observations .= process_member_data(SimDir(joinpath(obs_output_dir, "model_config/output_active/clima_atmos")))
     JLD2.save_object(obs_path, observations)
 end
+observations = JLD2.load_object(obs_path)
+@show observations
+u0_mean = [mean(prior)]
+uu0_cov = cov(prior)
+eki = EKP.EnsembleKalmanProcess(
+    EKP.ObservationSeries(EKP.Observation(observations, noise, "rsut")),
+    EKP.Unscented(u0_mean, uu0_cov),
+)
+ensemble_size = EKP.get_N_ens(eki)
 
-# Initialize experiment data
-@everywhere observations = JLD2.load_object(obs_path)
-
-eki = CAL.calibrate(CAL.WorkerBackend, ensemble_size, n_iterations, observations, noise, prior, output_dir)
+eki = CAL.calibrate(CAL.WorkerBackend, eki, n_iterations, prior, output_dir)
 
 # Postprocessing
 import EnsembleKalmanProcesses as EKP

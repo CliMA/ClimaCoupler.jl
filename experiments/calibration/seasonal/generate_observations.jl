e263a9f3b52f661abffc8117f71ddb5178bf1df7
nefrathenrici <nat.henrici@gmail.com>
2025-10-03 09:32:58 -0700

    init files

---
+++ experiments/calibration/seasonal/generate_observations.jl
@@ -0,0 +1,125 @@
+import ClimaAnalysis
+import ClimaAnalysis: OutputVar
+import ClimaCalibrate
+import Dates
+import EnsembleKalmanProcesses as EKP
+import JLD2
+import ClimaDiagnostics
+import ClimaCore
+# Access CalibrateConfig
+include(joinpath(@__DIR__, "run_calibration.jl"))
+
+include(joinpath(pkgdir(ClimaCoupler), "experiments/ClimaEarth/leaderboard/data_sources.jl"))
+
+function make_observation_vector(
+    short_names,
+    sample_date_ranges,
+    obsdir,
+    config_file,
+)
+    vars = load_vars(obsdir, short_names)
+    vars = preprocess_vars(vars, sample_date_ranges, config_file)
+    return make_observation_vector(vars, sample_date_ranges)
+end
+
+set_short_name!(var, short_name) = (var.attributes["short_name"] = short_name)
+
+"""
+    load_vars(obsdir, short_names)
+Load NetCDF files belonging to `short_names` in `obsdir` as `OutputVar`s.
+"""
+function load_vars(obsdir, short_names)
+    rad_and_pr_obs_dict = get_obs_var_dict()
+    rsut = rad_and_pr_obs_dict["rsut"](start_date)
+    # rsutcs = rad_and_pr_obs_dict["rsutcs"](start_date)
+    # sw_cre = rsutcs - rsut
+    set_short_name!(rsut, "rsut")
+    return [rsut]
+end
+
+"""
+    preprocess_vars(vars, sample_date_ranges, config_file)
+Preprocess each OutputVar in `vars` by keeping the relevant dates in
+`sample_date_ranges`.
+"""
+function preprocess_vars(vars, sample_date_ranges, config_file)
+    out_var = OutputVar("/glade/u/home/zhaoyi/clima_output/rsut_1M_average.nc")
+    resample_var(x) = ClimaAnalysis.resampled_as(x, out_var ; dim_names = ["lon", "lat"])
+    vars = resample_var.(vars)
+    vars = map(vars) do var
+        set_units(var, "W m^-2")
+    end
+    return vars
+end
+
+var_units = Dict(
+    "pr" => "kg m^-2 s^-1",
+    "mslp" => "Pa",
+    "tas" => "K",
+    "rsut" => "W m^-2",
+    "sw_cre" => "W m^-2"
+)
+
+
+function make_observation_vector(vars, covariance_sample_ranges)
+    obs_vec = map(CALIBRATE_CONFIG.sample_date_ranges) do sample_date_range
+        covar_estimator = ClimaCalibrate.ObservationRecipe.ScalarCovariance(
+            scalar = 1.0,
+            use_latitude_weights = true,
+        )
+        ClimaCalibrate.ObservationRecipe.observation(
+            covar_estimator,
+            vars,
+            first(sample_date_range),
+            last(sample_date_range),
+        )
+    end
+    return obs_vec
+end
+
+"""
+    resampled_lonlat(config_file)
+Return a function to resample longitude and latitudes according to the model
+grid specified by `config_file`.
+"""
+function resampled_lonlat(config_file)
+    ENV["CLIMACOMMS_DEVICE"] = "CPU"
+
+    cs = CoupledSimulation(config_file)
+    ENV["CLIMACOMMS_DEVICE"] = "CUDA"
+    center_space = cs.model_sims.atmos_sim.domain.center_space
+    (lon_nlevels, lat_nlevels, z_nlevels) =
+        ClimaDiagnostics.Writers.default_num_points(center_space)
+    longitudes = range(-180, 180, lon_nlevels)
+    latitudes = range(-90, 90, lat_nlevels)
+    stretch = center_space.grid.vertical_grid.topology.mesh.stretch
+    # TODO: Account for stretch for 3D variables and interpolate to pressure?
+    dz_bottom = center_space.grid.vertical_grid.topology.mesh.faces[2].z
+    z_levels = range(dz_bottom, ClimaCore.Spaces.z_max(center_space), z_nlevels)
+    return var -> resampled_as(var; lon = longitudes, lat = latitudes)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    sample_date_ranges = CALIBRATE_CONFIG.sample_date_ranges
+    short_names = CALIBRATE_CONFIG.short_names
+    config_file = CALIBRATE_CONFIG.config_file
+    @info "Generating observations for $short_names"
+    @info "The number of samples is $(length(sample_date_ranges)) over $sample_date_ranges"
+
+    # TODO: Use resample func
+    start_date = DateTime(2009,9,1)
+
+    unprocessed_vars = load_vars(nothing, short_names)
+    preprocessed_vars = preprocess_vars(unprocessed_vars, sample_date_ranges, config_file)
+    JLD2.save_object(
+        joinpath(pkgdir(ClimaCoupler),"experiments/calibration/era5_preprocessed_vars.jld2"),
+        preprocessed_vars,
+    )
+    covariance_sample_ranges = [(DateTime(yr, 3, 1), DateTime(yr, 5, 1)) for yr in 2001:2019]
+    observation_vector = make_observation_vector(preprocessed_vars, covariance_sample_ranges)
+    JLD2.save_object(
+        joinpath(pkgdir(ClimaCoupler),"experiments/calibration/weatherquest_obs_vec.jld2"),
+        observation_vector,
+    )
+end
---
+++ experiments/calibration/seasonal/run_calibration.jl
@@ -0,0 +1,99 @@
+using Dates
+using Distributed
+import Random
+import ClimaCalibrate
+import ClimaAnalysis
+import ClimaComms
+import ClimaCoupler
+import EnsembleKalmanProcesses as EKP
+import EnsembleKalmanProcesses.ParameterDistributions as PD
+import JLD2
+
+include(joinpath(pkgdir(ClimaCoupler), "experiments", "calibration", "api.jl"))
+include(joinpath(pkgdir(ClimaCoupler), "experiments/calibration/seasonal/observation_map.jl"))
+model_interface = joinpath(pkgdir(ClimaCoupler), "experiments", "calibration", "seasonal", "model_interface.jl")
+include(model_interface)
+years = 2010
+sample_date_ranges = repeat([(DateTime(yr, 3, 1), DateTime(yr, 5, 1)) for yr in years], 5)
+
+const CALIBRATE_CONFIG = CalibrateConfig(;
+config_file = joinpath(pkgdir(ClimaCoupler), "experiments/calibration/seasonal/amip_config.yml"),
+    short_names = ["rsut", ],
+    minibatch_size = 1,
+    n_iterations = 5,
+    sample_date_ranges,
+    extend = Dates.Week(7),
+    spinup = Dates.Day(14),
+    # TODO: Use this in the model_interface
+    output_dir = "/glade/derecho/scratch/zhaoyi/tmp/amip_calibration",
+    rng_seed = 42,
+)
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+
+    priors = [
+        #EKP.constrained_gaussian("prescribed_cloud_droplet_number_concentration", 3e8, 5e7, 1e7, 1e9)
+        #EKP.constrained_gaussian("ice_cloud_effective_radius", 4e-5, 5e-6, 5e-6, 9e-5)
+        EKP.constrained_gaussian("precipitation_timescale", 600, 100, 300, 1800)
+        EKP.constrained_gaussian("entr_inv_tau", 0.002, 0.0005, 0.0001, 0.01)
+    ]
+    prior = EKP.combine_distributions(priors)
+
+    observation_vector =
+        JLD2.load_object(joinpath(pkgdir(ClimaCoupler),"experiments/calibration/weatherquest_obs_vec.jld2"))
+
+    sample_date_ranges = CALIBRATE_CONFIG.sample_date_ranges
+    minibatch_size = CALIBRATE_CONFIG.minibatch_size
+    obs_series = EKP.ObservationSeries(
+        Dict(
+            "observations" => observation_vector,
+            "names" => [
+                string(Dates.year(start_date)) for
+                (start_date, stop_date) in sample_date_ranges
+            ],
+            "minibatcher" => ClimaCalibrate.minibatcher_over_samples(
+                length(observation_vector),
+                minibatch_size,
+            ),
+        ),
+    )
+
+    rng_seed = CALIBRATE_CONFIG.rng_seed
+    rng = Random.MersenneTwister(rng_seed)
+
+    ekp = EKP.EnsembleKalmanProcess(
+        obs_series,
+        EKP.TransformUnscented(prior, impose_prior = true);
+        verbose = true,
+        rng,
+        scheduler = EKP.DataMisfitController(terminate_at = 100),
+    )
+
+    # Using ETKI
+    # initial_ensemble = EKP.construct_initial_ensemble(rng, prior, 10)
+    # ekp = EKP.EnsembleKalmanProcess(
+    #     initial_ensemble,
+    #     obs_series,
+    #     EKP.TransformInversion(prior);
+    #     verbose = true,
+    #     rng,
+    #     scheduler = EKP.DataMisfitController(terminate_at = 100),
+    # )
+
+    hpc_kwargs = ClimaCalibrate.kwargs(time = 60*12,
+        ntasks = 2,
+        gpus_per_task = 1,
+        cpus_per_task = 4,
+        l_job_priority = "premium",
+        q = "main")
+    exeflags = "--threads=4"
+    eki = ClimaCalibrate.calibrate(ClimaCalibrate.DerechoBackend,
+        ekp,
+        CALIBRATE_CONFIG.n_iterations,
+        prior,
+        CALIBRATE_CONFIG.output_dir; 
+        model_interface, hpc_kwargs, exeflags
+    )
+
+end

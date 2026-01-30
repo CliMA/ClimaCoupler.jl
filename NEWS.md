56f1206985f4f41a7f3707a15aa56f45091f6f59
Akshay Sridhar <akshaysridhar@Akshays-MacBook-Air-10.local>
2026-01-29 19:53:26 -0800

    Use COARE3 parameterization over oceans.

--- NEWS.md
+++ NEWS.md
@@ -6,6 +6,8 @@ ClimaCoupler.jl Release Notes
 
 ### ClimaCoupler features
 
+#### Enable COARE3 roughness parameterization PR[#1689](https://github.com/CliMA/ClimaCoupler.jl/pull/1689) Allows :constant (land, ice) or :coare3 (ocean) aerodynamic roughness parameterization. 
+
 #### Rename abstract component types PR[#1688](https://github.com/CliMA/ClimaCoupler.jl/pull/1688)
 To follow abstract type naming convention. For example, `XModelSimulation` is now
 `AbstractXSimulation`.
--- src/Interfacer.jl
+++ src/Interfacer.jl
@@ -264,6 +264,21 @@ get_field(sim::AbstractSurfaceSimulation, ::Val{:emissivity}) =
 get_field(sim::AbstractSurfaceSimulation, ::Val{:height_disp}) =
     convert(eltype(sim.integrator.u), 0.0)
 
+"""
+    get_field(sim::AbstractSurfaceSimulation, ::Val{:roughness_model})
+
+Return the roughness model to be used for surface flux calculations.
+Returns `:constant` by default. Ocean models should override this to return
+`:coare3` to use COARE3 roughness parameterization, which accounts for the
+dynamic response of ocean surface roughness to wind speed and wave state.
+The `:constant` roughness model uses fixed roughness lengths for momentum
+and buoyancy specified by the component model.
+
+This ensures COARE3 is applied over ocean surfaces where the area_fraction > 0,
+while land and sea ice surfaces use constant roughness parameterization.
+"""
+get_field(::AbstractSurfaceSimulation, ::Val{:roughness_model}) = :constant
+
 
 """
     get_field(target_space, sim, quantity)

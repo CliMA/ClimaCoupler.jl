9ee5b614f3c03d7894ef818b1f6bf85263cb507b
Simone Silvestri <silvestri.simone0@gmail.com>
2026-02-13 09:13:27 +0100

    Apply suggestion from @simone-silvestri

--- ext/ClimaCouplerCMIPExt/oceananigans.jl
+++ ext/ClimaCouplerCMIPExt/oceananigans.jl
@@ -68,7 +68,9 @@ function OceananigansSimulation(
     Nz = 100
     depth = 6000 # meters
     z = OC.ExponentialDiscretization(Nz, -depth, 0; scale = 1800)
-    underlying_grid = OC.TripolarGrid(arch; size = (Nx, Ny, Nz), halo = (7, 7, 7), z)
+
+    # Regular LatLong because we know how to do interpolation there
+    underlying_grid = OC.TripolarGrid(arch; size = (Nx, Ny, Nz), halo = (7, 7, 7), z, fold_topology = OC.Grids.RightFaceFolded)
 
     bottom_height = CO.regrid_bathymetry(
         underlying_grid;

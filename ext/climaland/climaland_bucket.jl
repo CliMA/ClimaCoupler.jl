1d69348a2699d722dc1b821426a2d93dae3d952b
Julia Sloan <jsloan@caltech.edu>
2026-02-20 15:39:17 -0800

    fix bucket rad computation [skip ci]

--- ext/climaland/climaland_bucket.jl
+++ ext/climaland/climaland_bucket.jl
@@ -17,13 +17,11 @@ struct BucketSimulation{
     I <: SciMLBase.AbstractODEIntegrator,
     A <: CC.Fields.Field,
     OW,
-    RF,
 } <: Interfacer.AbstractLandSimulation
     model::M
     integrator::I
     area_fraction::A
     output_writer::OW
-    radiative_fluxes::RF
 end
 
 """
@@ -204,6 +202,9 @@ function BucketSimulation(
         start_date = tspan[1]
         stop_date = tspan[2]
     end
+
+    # Choose the timestepping algorithm
+    timestepper = CTS.ExplicitAlgorithm(CTS.RK4())
     simulation = CL.Simulations.LandSimulation(
         start_date,
         stop_date,
@@ -213,19 +214,10 @@ function BucketSimulation(
         set_ic!,
         user_callbacks = (),
         diagnostics,
+        timestepper,
     )
 
-    # TODO move these to ClimaLand.jl as part of CoupledRadiativeFluxes
-    radiative_fluxes =
-        (; SW_d = CC.Fields.zeros(surface_space), LW_d = CC.Fields.zeros(surface_space))
-
-    return BucketSimulation(
-        model,
-        simulation._integrator,
-        area_fraction,
-        output_writer,
-        radiative_fluxes,
-    )
+    return BucketSimulation(model, simulation._integrator, area_fraction, output_writer)
 end
 
 # extensions required by Interfacer
@@ -279,10 +271,10 @@ function Interfacer.update_field!(
     Interfacer.remap!(sim.integrator.p.drivers.P_liq, field ./ ρ_liq)
 end
 function Interfacer.update_field!(sim::BucketSimulation, ::Val{:SW_d}, field)
-    Interfacer.remap!(sim.radiative_fluxes.SW_d, field)
+    Interfacer.remap!(sim.integrator.p.drivers.SW_d, field)
 end
 function Interfacer.update_field!(sim::BucketSimulation, ::Val{:LW_d}, field)
-    Interfacer.remap!(sim.radiative_fluxes.LW_d, field)
+    Interfacer.remap!(sim.integrator.p.drivers.LW_d, field)
 end
 function Interfacer.update_field!(sim::BucketSimulation, ::Val{:air_temperature}, field)
     Interfacer.remap!(sim.integrator.p.drivers.T, field)
@@ -473,22 +465,8 @@ Updates the surface component model cache with the current coupler fields beside
 """
 function FieldExchanger.update_sim!(sim::BucketSimulation, csf)
     # radiative fluxes
-    # TODO add SW_d, LW_d fields to BucketSimulation and update there instead
     Interfacer.update_field!(sim, Val(:SW_d), csf.SW_d)
     Interfacer.update_field!(sim, Val(:LW_d), csf.LW_d)
-
-    model = sim.model
-    Y = sim.integrator.u
-    p = sim.integrator.p
-    t = sim.integrator.t
-
-    # Note: here we add negative signs to account for a difference in sign convention
-    #  between ClimaLand.jl and ClimaCoupler.jl in SW_d and LW_d.
-    σ = model.parameters.earth_param_set.Stefan
-    sim.integrator.p.bucket.R_n .=
-        .-(1 .- CL.surface_albedo(model, Y, p)) .* sim.radiative_fluxes.SW_d .-
-        Interfacer.get_field(sim, Val(:emissivity)) .*
-        (sim.radiative_fluxes.LW_d .- σ .* CL.component_temperature(model, Y, p) .^ 4)
 
     # precipitation
     Interfacer.update_field!(sim, Val(:liquid_precipitation), csf.P_liq)

59a7ece7c75ed20a7b50ecd2df6449c8df0c28ba
Julia Sloan <jsloan@caltech.edu>
2026-03-10 17:14:17 -0700

    enable checkpointing for CMIP

--- experiments/ClimaEarth/test/restart_state_only_cmip.jl
+++ experiments/ClimaEarth/test/restart_state_only_cmip.jl
@@ -1,15 +1,17 @@
-# This test runs a small AMIP simulation twice times.
+# This test runs a small CMIP simulation twice.
 #
-# - The first time the simulation is run for two steps
-# - The second time the simulation is run for two steps, but restarting from the
-#   first simulation
+# - The first time the simulation is run for two coupler steps
+# - The second time the simulation is run for two coupler steps, but restarting
+#   from the first simulation
 #
 # Since the caches are not read from the restart file, the results will not be
 # bit-wise identical to running the simulation without restarting. This test
-# only checks that a simulation can be restarted from the state only without erroring.
+# only checks that a simulation can be restarted from the state only without
+# erroring.
 #
-# The content of the simulation is not the most important, but it helps if it
-# has all of the complexity possible.
+# This test exercises the Oceananigans checkpointing path: checkpoint_model_state
+# writes a JLD2 file via OC.checkpoint, and restart_model_state! restores it
+# via OC.set!.
 
 import ClimaComms
 ClimaComms.@import_required_backends
@@ -36,19 +38,21 @@ tmpdir = ClimaComms.bcast(comms_ctx, tmpdir)
 maybe_wait_filesystem(ClimaComms.context(), tmpdir)
 
 # Parse the input config file as a dictionary
-config_file = joinpath(@__DIR__, "amip_test.yml")
+config_file = joinpath(@__DIR__, "restart_state_only_cmip.yml")
 config_dict = Input.get_coupler_config_dict(config_file)
 
-# Four steps
+# Two coupler steps
 two_steps = deepcopy(config_dict)
 
-two_steps["dt"] = "180secs"
-two_steps["dt_cpl"] = "180secs"
-two_steps["t_end"] = "360secs"
-two_steps["dt_rad"] = "180secs"
-two_steps["checkpoint_dt"] = "360secs"
+two_steps["dt_atmos"] = "120secs"
+two_steps["dt_cpl"] = "360secs"
+two_steps["dt_land"] = "120secs"
+two_steps["dt_ocean"] = "360secs"
+two_steps["dt_seaice"] = "360secs"
+two_steps["t_end"] = "720secs"
+two_steps["checkpoint_dt"] = "720secs"
 two_steps["coupler_output_dir"] = tmpdir
-two_steps["job_id"] = "two_steps"
+two_steps["job_id"] = "two_steps_cmip"
 two_steps["save_cache"] = false
 
 println("Simulating two steps")
@@ -65,18 +69,17 @@ end
 println("Simulating two steps, options from command line")
 two_steps_reading = deepcopy(two_steps)
 
-two_steps_reading["t_end"] = "540secs"
+two_steps_reading["t_end"] = "1080secs"
 two_steps_reading["detect_restart_files"] = true
 two_steps_reading["restart_dir"] = cs_two_steps.dir_paths.checkpoints_dir
-two_steps_reading["restart_t"] = 360
+two_steps_reading["restart_t"] = 720
 two_steps_reading["restart_cache"] = false
-two_steps_reading["job_id"] = "two_steps_reading"
+two_steps_reading["job_id"] = "two_steps_cmip_reading"
 two_steps_reading["save_cache"] = false
-# calling setup_and_run bypass get_coupler_config_dict which modifies the
-# t_start
+# calling setup_and_run bypasses get_coupler_config_dict which modifies t_start
 Input.update_t_start_for_restarts!(two_steps_reading)
 
 cs_two_steps_reading = setup_and_run(two_steps_reading)
-@testset "AMIP restarts (state only)" begin
+@testset "CMIP restarts (state only)" begin
     @test cs_two_steps_reading.tspan[1] == cs_two_steps.tspan[2]
 end

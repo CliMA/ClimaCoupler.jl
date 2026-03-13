1db4351094e22b145d7fc90b0a8a0596662ead7c
Julia Sloan <jsloan@caltech.edu>
2026-03-13 10:04:30 -0700

    fix atmos dss logic

--- ext/ClimaCouplerClimaAtmosExt.jl
+++ ext/ClimaCouplerClimaAtmosExt.jl
@@ -702,16 +702,20 @@ one here.
 """
 function dss_state!(sim::ClimaAtmosSimulation)
     Y = sim.integrator.u
-    if axes(Y.c.ρe_tot) isa CC.Spaces.PointSpace
-        return nothing
-    else
-        for key in propertynames(Y)
-            field = getproperty(Y, key)
-            buffer = Utilities.init_dss_buffer(field)
-            Utilities.apply_dss!(field, buffer)
-        end
+    # Skip DSS for PointSpace (e.g. SCM column mode). Y is (c=..., f=...) or flat NamedTuple of Fields.
+    first_val = getproperty(Y, first(propertynames(Y)))
+    leaf =
+        first_val isa CC.Fields.Field ? first_val :
+        getproperty(first_val, first(propertynames(first_val)))
+    if leaf isa CC.Fields.Field && axes(leaf) isa CC.Spaces.PointSpace
         return nothing
     end
+    for key in propertynames(Y)
+        field = getproperty(Y, key)
+        buffer = Utilities.init_dss_buffer(field)
+        Utilities.apply_dss!(field, buffer)
+    end
+    return nothing
 end
 
 """

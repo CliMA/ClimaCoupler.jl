55a79c44e3b496eaabbaf8e555767f6e4f51b2e3
nefrathenrici <nat.henrici@gmail.com>
2026-06-04 10:51:22 -0700

    revert to original config

--- experiments/calibration/amip/config/pressure_levels.jl
+++ experiments/calibration/amip/config/pressure_levels.jl
@@ -1,6 +1,6 @@
 # Define which coupler file to use
 config_file =
-    joinpath(pkgdir(ClimaCoupler), "config", "benchmark_configs", "amip_progedmf_1m_land_he16.yml")
+    joinpath(pkgdir(ClimaCoupler), "config", "amip_configs", "amip_calibration.yml")
 
 # Calibrate only on Jan 1 2010
 sample_date_ranges =
@@ -34,7 +34,7 @@ const NORMALIZATION_STATS_FP =
     joinpath(CALIBRATE_CONFIG.output_dir, "normalization_stats.jld2")
 
 const CALIBRATION_PRIORS = [
-    # PD.constrained_gaussian("precipitation_timescale", 1200, 300, 300, 2400),
+    PD.constrained_gaussian("precipitation_timescale", 1200, 300, 300, 2400),
     PD.constrained_gaussian("Tq_correlation_coefficient", 0.4, 0.4, -1.0, 1.0),
     PD.constrained_gaussian("mixing_length_eddy_viscosity_coefficient", 0.2, 0.1, 0, 1.0),
     PD.constrained_gaussian("mixing_length_diss_coeff", 0.22, 0.15, 0.0, 10.0),
